1d6502d76805dd259ad08781dc0209ed9a674668
Gaurav Karia <gkkaria09@gmail.com>
2025-04-29 18:40:09 +0530

    commit

--- index2.js
+++ index2.js
@@ -5,7 +5,6 @@ function Value() {
 
     localStorage.setItem("pl1n", pl1n);
     localStorage.setItem("pl2n", pl2n);
-    console.log(pl1n + "\n" + pl2n);
     window.location.href = "./game.html";
     
 }
